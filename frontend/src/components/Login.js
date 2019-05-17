4b881946ff0c550e0b6f3773a92023d67f49d016
Jason Boerner <jboerner56@gamil.com>
2019-05-17 07:43:26 -0400

    beginning form, handleChange

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -12,10 +12,29 @@ class Login extends React.Component{
         return (
             <div className="login">
             <form onSubmit={this.handleSubmit}>
-            
+            <label>Email</label>
+                <input 
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}/>
+                <label>Password</label>
+                <input
+                type="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                onSubmit={this.handleSubmit}/>
             </form>
             </div>
         )
+    }
+    handleChange(e) {
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+        console.log("handleChange", e.target.value)
+    }
+    handleSubmit(e){
+        e.preventDefault()
     }
 }
 
