9a558320bd2399dc4e94130611c6862f8a643d59
Jason Boerner <jboerner56@gamil.com>
2019-05-17 09:14:28 -0400

    react-router/path to login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 class Login extends React.Component{
     constructor(props) {
         super(props);
@@ -11,18 +10,27 @@ class Login extends React.Component{
     render() {
         return (
             <div className="login">
+                <h1 className="login-title">Login</h1>
             <form onSubmit={this.handleSubmit}>
             <label>Email</label>
                 <input 
                 type="email"
+                placeholder="email"
                 value={this.state.email}
                 onChange={this.handleChange}/>
                 <label>Password</label>
                 <input
                 type="password"
+                placeholder="password"
                 value={this.state.password}
-                onChange={this.handleChange}
-                onSubmit={this.handleSubmit}/>
+                onChange={this.handleChange}/>
+                <button 
+                type="submit"
+                onSubmit={this.handleSubmit}
+                disabled={!this.validateForm}
+                >
+                Submit
+                </button>
             </form>
             </div>
         )
@@ -36,6 +44,9 @@ class Login extends React.Component{
     handleSubmit(e){
         e.preventDefault()
     }
+    // validateForm() {
+    //     return this.state.email.length > 0 && this.state.password.length > 0;
+    // }
 }
 
 export default Login;
