7eb6936c8603a6bfee84235b73928b334a5672e4
victor truong <2truong93@gmail.com>
2019-05-21 12:11:44 -0400

    adds grid for svg drawing and health bars

--- frontend/src/components/Background.js
+++ frontend/src/components/Background.js
@@ -2,16 +2,57 @@ import React from 'react'
 
 export default function Background() {
     const style = {
-        fill: '#0E24EB',
+        stroke:'#e71818',
+        strokeWidth: '5',
+        fill: '#FFFFFF',
+        fillOpacity: '0',
+    }
+    const healthStyle = {
+        stroke:'black',
+        strokeWidth: '2',
+        fill: 'green',
+        fillOpacity: '100',
     }
   return (
-    <svg width="1000" height="1000">
-        <rect 
-        x="100"
-        y="100"
-        style={style}
-        width="500"
-        height="500"/>
+    <svg width="5000" height="5000" >
+    {/* SVG GRID BOX */}
+    <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <pattern id="smallGrid" width="8" height="8" patternUnits="userSpaceOnUse">
+            <path d="M 8 0 L 0 0 0 8" fill="none" stroke="gray" stroke-width="0.5"/>
+            </pattern>
+            <pattern id="grid" width="80" height="80" patternUnits="userSpaceOnUse">
+            <rect width="80" height="80" fill="url(#smallGrid)"/>
+            <path d="M 80 0 L 0 0 0 80" fill="none" stroke="gray" stroke-width="1"/>
+            </pattern>
+        </defs>
+
+        <rect width="100%" height="100%" fill="url(#grid)" />
     </svg>
+    {/* SVG GRID BOX END */}
+    <g
+    x="500"
+    y="500">
+        <rect 
+        x="0"
+        y="0"
+        style={style}
+        width="1200"
+        height="900"/>
+        <rect style={healthStyle}
+        x="10"
+        y="50"
+        width="450"
+        height="50"
+        />
+        <rect style={healthStyle}
+        x="740"
+        y="50"
+        width="450"
+        height="50"
+        />
+    </g>
+    </svg>
+    
   )
 }
