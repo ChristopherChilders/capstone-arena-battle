dc2d72c256a3e35936a5cb760c97c8fe6766edd7
Andrew Josey <JoganLosey@gmail.com>
2019-05-28 15:01:20 -0400

    css modules for the landing,login, and registration page done

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -3,6 +3,7 @@ import style from '../StyleSheets/LandingPage.module.css'
 import { 
     Link
 } from 'react-router-dom'
+import Emrboider from '../Images/TitleEmbroider2.png'
 
 
 class LandingPage extends React.Component {
@@ -14,14 +15,16 @@ class LandingPage extends React.Component {
         return (
             <div className={style.HeroImage}>
                 <div className={style.title}>
-                    <h1> Lorem Title Ipsum </h1>
+                    <h1> B R A W L  </h1>
+                        <img src={Emrboider}></img>
                 </div>
+
                 <div className={style.Login}>
                     <h1>
-                        <Link to='login'> Log in </Link>
+                        <Link to='login'> --Log in-- </Link>
                     </h1>
                     <h2>
-                    <Link to='registration'> Register an account</Link>
+                    <Link to='registration'> --Register an account-- </Link>
                     </h2>
                 </div>
             </div>
